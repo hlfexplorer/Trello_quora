195e016a716399188cd1f9325b42896f413d6fc1
ArchanaArige <nigam_archana@yahoo.co.in>
2019-11-01 02:36:52 +0530

    Added the getQuestionByUuid() and deleteQuestion()in the QuestionDao class

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -1,4 +1,33 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public class QuestionDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    /** comments by Archana **/
+    //This method retrieves the question based on question uuid, if found returns question else null
+    public QuestionEntity getQuestionByUuid(final String uuid) {
+        try {
+            return entityManager.createNamedQuery("QuestionByUuid", QuestionEntity.class).setParameter("uuid", uuid).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    /** comments by Archana **/
+    //This method deletes the question record from database
+    public void deleteQuestion(final QuestionEntity questionEntity) {
+        entityManager.remove(questionEntity);
+    }
+
 }
+
