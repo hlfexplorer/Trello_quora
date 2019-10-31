6dffeaccccf8ace131c965a29834133d87245c0a
ArchanaArige <nigam_archana@yahoo.co.in>
2019-11-01 02:28:23 +0530

    file got deleted due to merge conflits so added the logic again to QuestionController

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -1,27 +1,28 @@
 package com.upgrad.quora.api.controller;
 
-
 import com.upgrad.quora.api.model.QuestionDeleteResponse;
-import com.upgrad.quora.api.model.UserDeleteResponse;
 import com.upgrad.quora.service.business.QuestionBusinessService;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.MediaTypeEditor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/")
 public class QuestionController {
 
-@Autowired
-QuestionBusinessService questionBusinessService;
+    @Autowired
+    QuestionBusinessService questionBusinessService;
     /**Commets by Archana **/
     //The admin or the owner of the Question has a privilege of deleting the question
     //This endpoint requests for the questionUuid to be deleted and the questionowner or admin accesstoken in the authorization header
+
+
     @RequestMapping(method = RequestMethod.DELETE, path = "/question/delete/{questionId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionDeleteResponse> deleteQuestion(@PathVariable("questionId") final String questionUuid, @RequestHeader("authorization") final String authorization)throws AuthorizationFailedException, InvalidQuestionException {
 
