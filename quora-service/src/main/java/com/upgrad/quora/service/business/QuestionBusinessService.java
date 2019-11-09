5b616630ef7b50b3ff6061719c52bdf2c83280b8
ArchanaArige <nigam_archana@yahoo.co.in>
2019-11-09 18:04:39 +0530

    Pulling the Avia request from terminal for the endpoints editQuestion and getAllQuestionsbyUser

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -5,8 +5,10 @@ import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -24,8 +26,12 @@ public class QuestionBusinessService {
     @Autowired
     QuestionDao questionDao;
 
+    @Autowired
+    UserBusinessService userBusinessService;
+
 
     /** comments by Archana **/
+    //**postQuestion**//
     //This method receives questionEntity and accessToken as input parameters for creating the question, but before that
     // two checks are done whether a user is signedin or signedout. After successfull validation the question will be persisted
     //in the database by calling persistQuestion() method of QuestionDao
@@ -44,6 +50,7 @@ public class QuestionBusinessService {
     }
 
     /** comments by Archana **/
+    //**getAllQuestions**//
     //This method fetches all the questions that are posted in the application, but before that two checks are done
     //whether a user is signedin or signedout. After successfull validation, a user can view all the list of questions
       public List<QuestionEntity> getAllQuestions(final String accessToken) throws AuthorizationFailedException {
@@ -60,12 +67,12 @@ public class QuestionBusinessService {
 
 
 
-
+    /** comments by Archana **/
+    //**deleteQuestion**//
     @Transactional(propagation = Propagation.REQUIRED)
    public String deleteQuestion(final String questionUuid, final String accessToken) throws AuthorizationFailedException, InvalidQuestionException {
        UserAuthTokenEntity userAuthToken = userDao.getUserAuthToken(accessToken);
 
-       /** comments by Archana **/
        //If the accessToken of admin or QuestionOwner doesnt exist in the database throw following Exception
        //It means that if the user hasnt signedin, then the basic Authentication is not done and the accessToken is not generated
        if (userAuthToken == null) {
@@ -97,4 +104,70 @@ public class QuestionBusinessService {
 
         return questionUuid;
    }
+
+    /** comments by Avia **/
+    //This method retrieves the question in the database
+    public QuestionEntity getQuestion(final String questionUuid, final String accessToken) throws InvalidQuestionException{
+
+        QuestionEntity questionEntity = questionDao.getQuestionByUuid(questionUuid);
+        if(questionEntity == null) {
+            throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+        }
+        return questionEntity;
+
+    }
+
+    /** comments by Avia **/
+    //** editQuestion **//
+    //This method updates the question in the database
+    //THe method first checks if the user token is valid
+    //Next it checks if the user trying to edit is the owner of the question.
+    //If the current user is not the owner it throws an exception, else the question is updated
+    public QuestionEntity editQuestion(final QuestionEntity questionEntity, final String accessToken) throws AuthorizationFailedException {
+        UserAuthTokenEntity userAuthToken = userDao.getUserAuthToken(accessToken);
+        if (userAuthToken == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        ZonedDateTime logoutTime = userAuthToken.getLogoutAt();
+        if(logoutTime!= null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
+        }
+
+        String questionOwnerUuid = questionEntity.getUser().getUuid();
+        String signedInUserUuid = userAuthToken.getUser().getUuid();
+
+        if (questionOwnerUuid.equals(signedInUserUuid)) {
+            QuestionEntity updatedQuestion = questionDao.updateQuestion(questionEntity);
+            return updatedQuestion;
+        }
+
+        else{
+            throw new AuthorizationFailedException("ATHR-003","Only the question owner can edit the question");
+        }
+
+    }
+
+    /** comments by Avia **/
+    //** getAllQuestionsByUser **//
+    //This method updates the question in the database retrieves all the questions post by a user from the database
+
+    public List<QuestionEntity> getAllQuestionsByUser(final String accessToken, String userUuid) throws AuthorizationFailedException, UserNotFoundException {
+        UserAuthTokenEntity userAuthToken = userDao.getUserAuthToken(accessToken);
+        if (userAuthToken == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        ZonedDateTime logoutTime = userAuthToken.getLogoutAt();
+        if(logoutTime!= null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
+        }
+
+        UserEntity userEntity = userBusinessService.getUser(userUuid, accessToken);
+        if (userEntity == null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid whose question details are to be seen does not exist");
+        }
+
+        return questionDao.getAllQuestionsByUser(userUuid);
+
+
+    }
 }
